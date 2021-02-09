40bb128b9ee43d099d3fa759358631166e15c2b5
Caio Rocha <caiorochapstrlrd@gmail.com>
2021-02-09 17:12:53 -0300

    refactor(isSorted): remove unnecessary expression at if

--- fp-in-scala/chapter-2/exercise2.2/IsSorted.scala
+++ fp-in-scala/chapter-2/exercise2.2/IsSorted.scala
@@ -2,7 +2,7 @@ object IsSorted {
     def isSorted[A](as: Array[A], ordered: (A,A) => Boolean): Boolean = {
         @annotation.tailrec
         def loop(n: Int, assertion: Boolean): Boolean = 
-            if (n+1 == as.length-1 && ordered(as(n), as(n+1))) assertion
+            if (n+1 == as.length-1) assertion
             else loop(n+1, assertion && ordered(as(n), as(n+1)))
         loop(0, true)
     }
